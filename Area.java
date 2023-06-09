9367ff381e972f3c5f9a4bfba32d7d19c618ae33
dharma <nandyaladharmateja123@gmail.com>
2023-06-09 14:54:01 +0530

    area

---
+++ Area.java
@@ -0,0 +1,20 @@
+package com.practice;
+
+import java.util.Scanner;
+
+public class Area {
+	 public static void main(String[] args) {
+		 String username="dharma Teja";
+		 int paswd=8765;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("please enter the user name");
+		String us=sc.nextLine();
+		System.out.println("good mrng"+username);
+		
+		
+		System.out.println("please enter the passwd");
+		int pasd=sc.nextInt();
+		System.out.println("welcome");
+	}
+	
+}
